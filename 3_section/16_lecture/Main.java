d44c0366712cd9b92126b16d75b8f72c9c07011f
anantvardhanpandey <dkpandeycan1@gmail.com>
2025-07-10 23:34:05 +0530

    edited section 3 lecture 16

--- 3_section/16_lecture/Main.java
+++ 3_section/16_lecture/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!, this is 15th lecture of section 3");
+        System.out.println("Hello, World!, this is 16th lecture of section 3");
+        int myVariable = 10;
+        System.out.println(myVariable);
+        System.out.println("myVariable");
+
     }
 }
