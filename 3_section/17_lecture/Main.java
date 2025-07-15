8dc615d3e93ddbe090f8d9683ce64634730c67fa
anantvardhanpandey <dkpandeycan1@gmail.com>
2025-07-15 11:50:19 +0530

    lecture-17 variables

--- 3_section/17_lecture/Main.java
+++ 3_section/17_lecture/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!, this is 17 lecture of section 3");
+        int myFirstVariable= (10+5)+(2*10);
+        int mySecondVariable = 12;
+        int myThirdVariable = 6;
+        int myTotal = myFirstVariable + mySecondVariable + myThirdVariable;
+        System.out.println(myTotal);
+        
+        
     }
 }
