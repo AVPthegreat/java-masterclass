9cc46bf7cf3e9a88786bfe7c3fcb1b57740a0112
anantvardhanpandey <dkpandeycan1@gmail.com>
2025-09-08 14:11:49 +0530

    updated

--- 3_section/20_lecture/Main.java
+++ 3_section/20_lecture/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!, this is 20th lecture of section 3");
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
     }
 }
