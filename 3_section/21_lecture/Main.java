cd0521b2716b57bf2868dc49160566f0e9c7eb6f
anantvardhanpandey <dkpandeycan1@gmail.com>
2025-09-09 11:14:36 +0530

    completed

--- 3_section/21_lecture/Main.java
+++ 3_section/21_lecture/Main.java
@@ -2,6 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!, this is 21st lecture of section 3");
-        
+        byte var1=10;
+        short var2=20;
+        int var3=30;
+        int sum = var1 + var2 + var3;
+        long var4 = 50000L + (10 * sum);
+        System.out.println("The value of long variable is: " + var4);
+        //long var4 = (long)(50000 + 10 * (var1 + var2 + var3));
+        //long var4 = 50000L + 10L * (var1 + var2 + var3);
+
+
+        //✅ Rule of thumb:
+        //When going from smaller → bigger (like int → long), Java does it automatically.
+        //When going from bigger → smaller (long → int), you need an explicit cast.
     }
 }
