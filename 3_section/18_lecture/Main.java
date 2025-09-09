2d3bfb02d6b2fbc18eeeb3f9c39a0aacb733d4c7
anantvardhanpandey <dkpandeycan1@gmail.com>
2025-09-09 10:00:43 +0530

    updated lecture 18 notes

--- 3_section/18_lecture/Main.java
+++ 3_section/18_lecture/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!, this is 17th lecture of section 3");
+        System.out.println("Hello, World!, this is 18th lecture of section 3");
         int myIntVar = 1000;
         System.out.println(myIntVar);
 
